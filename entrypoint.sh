2cc2daa14d6a954da89fd7e068098d4411329213
Maxime DAVID <got2bex@gmail.com>
2020-03-16 20:13:04 -0400

    go

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-docker build . -f /Dockerfile.buildNativeApp -t native-build-image
+set -e
+TAG=$1
+
+if [ -z $TAG ]; then
+  echo '<tag> parameter is missing'
+  exit 1
+fi
+
+chmod u+x native-executable/app-runner
+docker build . -f /Dockerfile.dockerize -t $TAG
+
+echo ::set-output name=image::$TAG
