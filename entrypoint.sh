eb8ca97408bab515942e4e99a96d374734505cd7
Maxime David <contact@maximedavid.fr>
2020-03-16 20:20:05 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
 set -e
-TAG=$1
 
-if [ -z $TAG ]; then
-  echo '<tag> parameter is missing'
-  exit 1
-fi
-
-chmod u+x native-executable/app-runner
-docker build . -f /Dockerfile.dockerize -t $TAG
-
-echo ::set-output name=image::$TAG
+docker build . -f /Dockerfile.buildNativeApp -t native-build-image
+id=$(docker create native-build-image)
+docker cp $id:/usr/src/app/target/app-runner app-runner
