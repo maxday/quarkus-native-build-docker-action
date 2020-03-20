d303260eae8c4f9609d3db19b7cebf62fdc69a39
Maxime DAVID <contact@maximedavid.fr>
2020-03-20 15:05:58 -0400

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 set -e
-pwd
-ls -la
-ls -la /github/workspace
-docker build . -f /Dockerfile -t native-build-image
+
+DOCKERFILE_PATH=$1
+DOCKER_FILE_TO_USE="/Dockerfile"
+
+if [ ! -z $DOCKERFILE_PATH  &&  -f $DOCKERFILE_PATH ]; then
+  echo "Impossible to find the Dockerfile at $DOCKERFILE_PATH"
+  exit 1
+else
+  DOCKER_FILE_TO_USE=$DOCKERFILE_PATH
+fi
+
+docker build . -f $DOCKER_FILE_TO_USE -t native-build-image
 id=$(docker create native-build-image)
 docker cp $id:/usr/src/app/target/app-runner app-runner
